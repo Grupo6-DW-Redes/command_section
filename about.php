3d30738ef35275f2dfe7a31e089db476fcc5915d
jonniojen <jonniojen@gmail.com>
2013-04-17 09:54:08 -0300

    Create about.php
    Pagina sobre o site e a equipe do projeto.

---
+++ about.php
@@ -0,0 +1,22 @@
+<style type="text/css">
+  
+	#cabecalho {
+		background-color: RoyalBlue;
+		color: white;
+		text-transform: uppercase;
+		font-size: 18px;
+		font-family: Arial,Helvetica,sans-serif;
+	}
+
+</style>
+
+
+<h1 id="cabecalho">TOOLS FOR NETWORK TESTING</h1>
+<h3>O QUE É ESTA FERRAMENTA??</h3>
+<p>Aplicação, sob a forma de página web, para ajudar na validação dos serviços relacionados ao segmento de Redes de Computadores.</p>
+<h3>QUEM PODE USAR??</h3>
+<p>Usuários domésticos e profissionais da área de Computação que buscam identificar problemas e  anomalias em sua rede doméstica ou intranet.</p>
+<h3>QUAIS AS VANTAGENS DE SEU USO??</h3>
+<p>=>Curva de aprendizado muito rápida<br>=>Acesso de qualquer dispositivo que tenha conexão com a Internet<br>=>Não precisa digitar comandos de terminal ou prompt<br>=>Pode ser expandido para agregar novas funcionalidades</p>
+<h3>PROGRAMADORES:</h3>
+<p>Jonnio Guimaraes<br>Pedro Damião<br>Victor Ricardo</p>
